ed76ebd2b8854a8e3984ff67a1f2508f76ea32c9
Lara Corsini <laracorsinidemiranda@gmail.com>
2026-05-31 02:15:39 -0300

    feat: Adiciona mecânica de diminuir o botão na quarta tentativa

--- Assets/Scripts/Systems/AttemptSystem.cs
+++ Assets/Scripts/Systems/AttemptSystem.cs
@@ -8,16 +8,29 @@ public class AttemptSystem : MonoBehaviour
     [SerializeField] private Transform simButton;
     [SerializeField] private Transform naoButton;
     [SerializeField] private SwapZone swapZone;
+
+    [SerializeField] private Transform tinyNaoPosition;
+
+    // Escala original do botão NÃO
+    private Vector3 originalNaoScale;
+
+    // Última tentativa
     private int lastAppliedAttempt = -1;
 
+    // Posições originais dos botões
     private Vector3 originalSimPosition;
     private Vector3 originalNaoPosition;
 
     private void Awake()
     {
         Instance = this;
+
+        // Define onde os botões estarão quando o jogo rodar
         originalSimPosition = simButton.position;
         originalNaoPosition = naoButton.position;
+
+        // Define a escala original do botão NÃO quando o jogo rodar
+        originalNaoScale = naoButton.localScale;
     }
 
     public void ApplyAttemptEffects(int naoAttempts)
@@ -31,21 +44,36 @@ public class AttemptSystem : MonoBehaviour
         {
             case 2:
                 break;
+
+            case 3:
+                MakeNaoButtonTiny();
+                break;
         }
     }
 
+    // Inverte a posição dos botões
     public void SwapButtons()
     {
         Vector3 tempPosition = simButton.position;
-
         simButton.position = naoButton.position;
-
         naoButton.position = tempPosition;
     }
 
+    // Diminui o tamanho do botão NÃO
+    private void MakeNaoButtonTiny()
+    {
+        naoButton.localScale =
+            new Vector3(0.2f, 0.2f, 1f);
+
+        naoButton.position =
+            tinyNaoPosition.position;
+    }
+
+    // Reseta os estados dos botões para os originais
     public void ResetAttemptEffects()
     {
         simButton.position = originalSimPosition;
         naoButton.position = originalNaoPosition;
+        naoButton.localScale = originalNaoScale;
     }
 }
