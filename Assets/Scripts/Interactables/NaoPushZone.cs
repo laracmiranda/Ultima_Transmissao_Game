12a028f2dd8da8b1c29eb757c98fd3ab46b127ce
Lara Corsini <laracorsinidemiranda@gmail.com>
2026-05-31 01:08:34 -0300

    feat: Adiciona mecânica do empurrão na segunda tentativa

---
+++ Assets/Scripts/Interactables/NaoPushZone.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+// Função para empurrar o player na segunda tentativa
+public class NaoPushZone : MonoBehaviour
+{
+
+    private bool hasPushedPlayer;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+
+        // Verifica se o player já foi empurrado
+        if (hasPushedPlayer)
+            return;
+
+        if (!other.CompareTag("Player"))
+            return;
+
+        // Verifica se é a segunda tentativa
+        if (GameManager.Instance.CurrentAttempt != 1)
+            return;
+
+        // Faz o "empurrão"
+        Vector2 direction =
+            (other.transform.position - transform.position).normalized;
+
+        other.transform.position +=
+            (Vector3)(direction * 2f);
+
+        // Marca que o player já foi empurrado e assim não repete o estado
+        hasPushedPlayer = true;
+    }
+}
