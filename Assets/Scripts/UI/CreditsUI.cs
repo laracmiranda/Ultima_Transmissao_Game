1be851efc32096c8972cfad74413bf179c2ffc23
Lara Corsini de Miranda <laracorsinidemiranda@gmail.com>
2026-06-10 00:15:14 -0300

    feat: adiciona efeito de rolagem no texto dos créditos

--- Assets/Scripts/UI/CreditsUI.cs
+++ Assets/Scripts/UI/CreditsUI.cs
@@ -1,24 +1,40 @@
 using UnityEngine;
+using TMPro;
+using System.Collections;
 
 public class CreditsUI : MonoBehaviour
 {
     public static CreditsUI Instance;
     private bool showingCredits;
 
+    private Vector2 initialCreditsPosition;
+
     private bool canExit;
 
-
     [SerializeField] private GameObject panel;
+
+    [SerializeField] private RectTransform creditsText;
+    [SerializeField] private GameObject quitText;
+    [SerializeField] private float scrollSpeed = 80f;
+    [SerializeField] private float targetY = 0f;
 
     private void Awake()
     {
         Instance = this;
+
+        initialCreditsPosition = creditsText.anchoredPosition;
     }
 
     public void Show()
     {
         panel.SetActive(true);
+
+        creditsText.anchoredPosition = initialCreditsPosition;
+
         showingCredits = true;
+        quitText.SetActive(false);
+        
+        StartCoroutine(ScrollCredits());
 
         canExit = false;
         Invoke(nameof(EnableExit), 0.3f);
@@ -56,5 +72,31 @@ public class CreditsUI : MonoBehaviour
         panel.SetActive(false);
         showingCredits = false;
         canExit = false;
+    }
+
+    private IEnumerator ScrollCredits()
+    {
+        while (
+            creditsText.anchoredPosition.y <
+            targetY
+        )
+        {
+            creditsText.anchoredPosition +=
+                Vector2.up *
+                scrollSpeed *
+                Time.deltaTime;
+
+            yield return null;
+        }
+
+        creditsText.anchoredPosition =
+            new Vector2(
+                creditsText.anchoredPosition.x,
+                targetY
+            );
+
+        quitText.SetActive(true);
+
+        EnableExit();
     }
 }
